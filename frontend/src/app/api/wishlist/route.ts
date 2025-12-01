681c643235b610629f713e566285b1b6a7adc44b
sukhdip.2479@gmail.com <81208102+Mystical123@users.noreply.github.com>
2025-11-30 19:04:57 -0800

    added backend features

--- frontend/src/app/api/wishlist/route.ts
+++ frontend/src/app/api/wishlist/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3001";
+const BACKEND_URL = process.env.BACKEND_URL ||  "https://library-management-system-backend.onrender.com";
 
 export async function GET() {
   try {
